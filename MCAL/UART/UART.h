bec5c59eb3c9bbcc183afc47571c1c43834bd85e
Mina Atef Tawfeq <minaaaatef@users.noreply.github.com>
2019-04-22 14:21:19 +0200

    Uart Header File

---
+++ MCAL/UART/UART.h
@@ -0,0 +1,39 @@
+#ifndef __UART_H__
+#define	__UART_H__
+
+
+#define boudRateDivider = 16
+#define Clk = 80000000 
+#define BAUDRATE(BaudRate) (Clk / (BaudRate*boudRateDivider)) 
+
+
+typedef enum {
+ uart0,uart1,uart2,uart3,uart4,uart5,uart6,uart7
+} UARTNUM;
+
+
+typedef enum {
+ OFF , ON
+} PARITY;
+
+
+typedef enum {
+ OFF , ON
+} INTERRUPT;
+
+
+void UART_Init(UARTNUM UartNum , u32 BaudRate , PARITY Parity , INTERRUPT interrupt );
+
+
+u8 UART_Available(UARTNUM UartNum);
+
+
+u8 UART_Read(UARTNUM UartNum);
+
+
+void UART_Write(UARTNUM UartNum,u8 data);
+
+void Uart_Print (UARTNUM UartNum,u8 data)
+
+#endif // __UART_H__
+
