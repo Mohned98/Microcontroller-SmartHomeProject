34bd4f5c71fdf4542cc78e4e494bac1a4a0eb437
mostafaezz97 <mostafaezz1997@yahoo.com>
2019-05-08 00:32:08 +0200

    uart_write update

--- MCAL/UART/UART_Write.c
+++ MCAL/UART/UART_Write.c
@@ -1,43 +1,46 @@
 #include "UART.h"
-void UART_Write(UARTNUM UartNum,u8 data){
+
+#define zero 0
+
+void UART_Write(UARTNUM UartNum,uint8 data){
 	switch(UartNum){
 		case 0:
-			while((UART0_FR_R&UART_FR_TXFF) != 0);
+			while((UART0_FR_R&UART_FR_TXFF) != zero);
 				UART0_DR_R = data;
 		break;
 		
 		case 1:
-			while((UART1_FR_R&UART_FR_TXFF) != 0);
+			while((UART1_FR_R&UART_FR_TXFF) != zero);
 				UART1_DR_R = data;
 		break;
 		
 		case 2:
-			while((UART2_FR_R&UART_FR_TXFF) != 0);
+			while((UART2_FR_R&UART_FR_TXFF) != zero);
 				UART2_DR_R = data;
 		break;
 		
 		case 3:
-			while((UART3_FR_R&UART_FR_TXFF) != 0);
+			while((UART3_FR_R&UART_FR_TXFF) != zero);
 				UART3_DR_R = data;
 		break;
 		
 		case 4:
-			while((UART4_FR_R&UART_FR_TXFF) != 0);
+			while((UART4_FR_R&UART_FR_TXFF) != zero);
 				UART4_DR_R = data;
 		break;
 		
 		case 5:
-			while((UART5_FR_R&UART_FR_TXFF) != 0);
+			while((UART5_FR_R&UART_FR_TXFF) != zero);
 				UART5_DR_R = data;
 		break;
 		
 		case 6:
-			while((UART6_FR_R&UART_FR_TXFF) != 0);
+			while((UART6_FR_R&UART_FR_TXFF) != zero);
 				UART6_DR_R = data;
 		break;
 		
 		case 7:
-			while((UART7_FR_R&UART_FR_TXFF) != 0);
+			while((UART7_FR_R&UART_FR_TXFF) != zero);
 				UART7_DR_R = data;
 		break;
 	}
