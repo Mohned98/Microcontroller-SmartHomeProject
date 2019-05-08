29695c5b2a1bbdb39ac86bd79ceb177725deb0ab
menna-hussien <31513435+menna-hussien@users.noreply.github.com>
2019-05-08 11:19:18 +0300

    Update Uart_Print.c

--- MCAL/UART/Uart_Print.c
+++ MCAL/UART/Uart_Print.c
@@ -1,73 +1,62 @@
 #include "UART.h"
-void Uart_Print (UARTNUM UartNum, const char *Str)
-{
-		
-
+void Uart_Print (UARTNUM UartNum, const char *Str){
 	uint8 i = 0;
+	
 	switch(UartNum){
-		
-		case 0:	
-		while(Str[i] != '\0')
-		{
-		UART_Write(0,Str[i]);
-		i++;
-		}
+		case uart0:	
+			while(Str[i] != '\0'){
+				UART_Write(uart0,Str[i]);
+				i++;
+			}
 		break;
 		
-		case 1:
-			while(Str[i] != '\0')
-		{
-		UART_Write(1,Str[i]);
-		i++;
-		}
+		case uart1:
+			while(Str[i] != '\0'){
+				UART_Write(uart1,Str[i]);
+				i++;
+			}
 		break;
 		
-		case 2:
-			while(Str[i] != '\0')
-		{
-		UART_Write(2,Str[i]);
-		i++;
-		}
+		case uart2:
+			while(Str[i] != '\0'){
+				UART_Write(uart2,Str[i]);
+				i++;
+			}
 		break;
 		
-		case 3:
-		while(Str[i] != '\0')
-		{
-		UART_Write(3,Str[i]);
-		i++;
-		}	
+		case uart3:
+			while(Str[i] != '\0'){
+				UART_Write(uart3,Str[i]);
+				i++;
+			}	
 		break;
 		
-		case 4:
-		while(Str[i] != '\0')
-		{
-		UART_Write(4,Str[i]);
-		i++;
-		}
+		case uart4:
+			while(Str[i] != '\0'){
+				UART_Write(uart4,Str[i]);
+				i++;
+			}
 		break;
 		
-		case 5:
-		while(Str[i] != '\0')
-		{
-		UART_Write(5,Str[i]);
-		i++;
-		}	
+		case uart5:
+			while(Str[i] != '\0'){
+				UART_Write(uart5,Str[i]);
+				i++;
+			}	
 		break;
 		
-		case 6:
-		while(Str[i] != '\0')
-		{
-		UART_Write(6,Str[i]);
-		i++;
-		}
+		case uart6:
+			while(Str[i] != '\0'){
+				UART_Write(uart6,Str[i]);
+				i++;
+			}
 		break;
 		
-		case 7:
-		while(Str[i] != '\0')
-		{
-		UART_Write(7,Str[i]);
-		i++;
-		}	
+		case uart7:
+			while(Str[i] != '\0'){
+				UART_Write(uart7,Str[i]);
+				i++;
+			}	
 		break;
 	}
 }
