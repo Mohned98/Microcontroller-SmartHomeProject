8a4a707240e99353a1203aa8b9c1507575fd4e05
mariam-ayman <36281501+mariam-ayman@users.noreply.github.com>
2019-05-09 16:04:13 +0200

    Act on Rising/Falling edge functions

---
+++ ECUAL/Button/pushbutton.c
@@ -0,0 +1,31 @@
+#include "button.h"
+
+void Button_ActOnRisingEdge( Dio_PortName port_name, uint8 pin_Number, void (*pAction)(void) ){
+	if(DIO_ReadPort(port_name,1<<pin_Number))
+	{
+		SysTick_delay_ms(50);
+		if(DIO_ReadPort(port_name,1<<pin_Number))
+		{
+			pAction();
+			while(DIO_ReadPort(port_name,1<<pin_Number))
+			{}
+		}
+	}
+	
+}
+
+void Button_ActOnFallingEdge( Dio_PortName port_name, uint8 pin_Number, void (*pAction)(void) )
+{
+	if(DIO_ReadPort(port_name,1<<pin_Number))
+	{
+		SysTick_delay_ms(50);
+		if(DIO_ReadPort(port_name,1<<pin_Number))
+		{
+			while(DIO_ReadPort(port_name,1<<pin_Number))
+			{}
+				pAction();
+		}
+	}
+	
+	
+}
