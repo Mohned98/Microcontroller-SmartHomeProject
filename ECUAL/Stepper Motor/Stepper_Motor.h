86cdea71fb18cf1d652b1a232638f1647092bf00
mostafaezz97 <mostafaezz1997@yahoo.com>
2019-05-10 23:34:37 +0200

    update of header file of stepper motor

--- ECUAL/Stepper Motor/Stepper_Motor.h
+++ ECUAL/Stepper Motor/Stepper_Motor.h
@@ -13,8 +13,8 @@ typedef enum
 	}Stepper_Pin_Color;
 	
 
-void StepperMotor_Init(Dio_PortName port_name);
+void StepperMotor_Init(Dio_PortName port_name,uint8 pins_mask);
 void StepperMotor_ClkWise(float32 cycle);
 void StepperMotor_AntiClkWise(float32 cycle);
-void StepperMotor_Stop(void);
+void StepperMotor_Stop(Dio_PortName port_name, uint8 pins_mask);
 #endif /* STEPPER_MOTOR_H_ */
