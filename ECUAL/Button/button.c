00feee5ce6dd0169c504ec247ede90659f39ed62
MariamSafwat <36268243+MariamSafwat@users.noreply.github.com>
2019-05-10 16:55:56 +0200

    button_init/ ActOnHighLevel/ ActOnLowLevel.c

---
+++ ECUAL/Button/button.c
@@ -0,0 +1,43 @@
+include"button.h"
+
+void Button_Init( Dio_PortName port_name, uint8 pins_mask, InputMode mode )
+{
+	DIO_init(port_name,pins_mask);
+	DIO_SetPinDirection(port_name, pins_mask, DIO_PIN_IN);
+	if(mode == PULL_DOWN)
+		DIO_SetPinPullDown(port_name, pins_mask);
+	else
+		DIO_SetPinPullUp(port_name, pins_mask);
+}
+
+void Button_ActOnHighLevel( Dio_PortName port_name, uint8 pin_Number, void (*pAction)(void) )
+{
+	if (DIO_ReadPort(port_name, 1<<pin_Number))
+	{
+		SysTick_delay_ms(30);
+		if(DIO_ReadPort(port_name, 1<<pin_Number))
+		{
+			while(DIO_ReadPort(port_name, 1<<pin_Number))
+			{
+				pAction();
+				SysTick_delay_ms(200);
+			}
+		}
+	}
+}
+
+void Button_ActOnLowLevel( Dio_PortName port_name, uint8 pin_Number, void (*pAction)(void) )
+{
+	if (!DIO_ReadPort(port_name, 1<<pin_Number))
+	{
+		SysTick_delay_ms(30);
+		if(!DIO_ReadPort(port_name, 1<<pin_Number))
+		{
+			while(!DIO_ReadPort(port_name, 1<<pin_Number))
+			{
+				pAction();
+				SysTick_delay_ms(200);
+			}
+		}
+	}
+}
