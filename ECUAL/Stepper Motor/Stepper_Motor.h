583256a722bd011a5078cc4d430f23dbd645eaae
Mona Mahmoud <monamahmoud1997@gmail.com>
2019-05-11 08:12:02 +0300

    Update Stepper_Motor.h

--- ECUAL/Stepper Motor/Stepper_Motor.h
+++ ECUAL/Stepper Motor/Stepper_Motor.h
@@ -14,7 +14,7 @@ typedef enum
 	
 
 void StepperMotor_Init(Dio_PortName port_name,uint8 pins_mask);
-void StepperMotor_ClkWise(float32 cycle);
-void StepperMotor_AntiClkWise(float32 cycle);
+void StepperMotor_ClkWise(Dio_PortName port_name, uint8 pins_mask,float32 cycle);
+void StepperMotor_AntiClkWise(Dio_PortName port_name, uint8 pins_mask,float32 cycle);
 void StepperMotor_Stop(Dio_PortName port_name, uint8 pins_mask);
 #endif /* STEPPER_MOTOR_H_ */
