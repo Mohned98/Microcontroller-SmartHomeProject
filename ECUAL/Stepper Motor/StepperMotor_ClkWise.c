a00fe5153b2215d0cea5933802771605a713328e
Mona Mahmoud <monamahmoud1997@gmail.com>
2019-05-11 08:14:04 +0300

    Create StepperMotor_ClkWise.c

---
+++ ECUAL/Stepper Motor/StepperMotor_ClkWise.c
@@ -0,0 +1,28 @@
+#include "Stepper_Motor.h"
+void StepperMotor_ClkWise(Dio_PortName port_name, uint8 pins_mask,float32 cycle)
+{
+	uint8 i=0;
+for (float32 j=0; j<cycle; j++)
+{
+	DIO_WritePort( port_name,  pins_mask, 1<<i);
+	 i++;             
+  SysTick_delay_ms(100); 
+  if (i==4)
+      i=0;
+}
+}
+
+
+
+void StepperMotor_AntiClkWise(Dio_PortName port_name, uint8 pins_mask,float32 cycle)
+{
+	uint8 i=0;
+for (float32 j=0; j<cycle; j++)
+{
+	DIO_WritePort( port_name,  pins_mask, 8>>i);
+	 i++;             
+  SysTick_delay_ms(100); 
+  if (i==4)
+      i=0;
+}
+}
