e4dc52c5807d4730c05e8f92e5570afd14d31a46
mostafaezz97 <mostafaezz1997@yahoo.com>
2019-05-14 19:50:34 +0200

    edit of stepper init and stop functions

--- ECUAL/Stepper Motor/stepper_init_stop.c
+++ ECUAL/Stepper Motor/stepper_init_stop.c
@@ -1,17 +1,14 @@
-
-
 #include "Stepper_Motor.h"
 
 
-void StepperMotor_Init(Dio_PortName port_name,uint8 pins_mask){
-	
-	DIO_Init(  port_name, pins_mask );
-	DIO_SetPinDirection(  port_name,  pins_mask, DIO_PIN_OUT );
-	
+void StepperMotor_Init()
+{
+	DIO_Init(STEPPER_PORT, STEPPER_MASK);
+	DIO_SetPinDirection( STEPPER_PORT, STEPPER_MASK, DIO_PIN_OUT );
+	DIO_WritePort(STEPPER_PORT,1<<STEPPER_RED, STD_HIGH);
 }
 
-void StepperMotor_Stop(Dio_PortName port_name, uint8 pins_mask){
-	
-	void DIO_WritePort( port_name,  pins_mask, STD_LOW);
-	
+void StepperMotor_Stop()
+{
+	 DIO_WritePort(STEPPER_PORT, STEPPER_MASK, STD_LOW);
 }
