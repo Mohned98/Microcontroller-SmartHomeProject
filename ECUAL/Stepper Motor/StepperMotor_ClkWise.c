648835cf1a0ce950d093b8cd6329c69a76185548
monaa12 <36414725+monaa12@users.noreply.github.com>
2019-05-14 18:51:27 +0300

    Update StepperMotor_ClkWise.c

--- ECUAL/Stepper Motor/StepperMotor_ClkWise.c
+++ ECUAL/Stepper Motor/StepperMotor_ClkWise.c
@@ -1,29 +1,67 @@
 #include "Stepper_Motor.h"
 #include "Systick.h"
-void StepperMotor_ClkWise(Dio_PortName port_name, uint8 pins_mask,float32 cycle)
+void StepperMotor_ClkWise(float32 cycle)
 {
 	uint8 i=0;
-for (float32 j=0; j<cycle; j++)
-{
-	DIO_WritePort( port_name,  pins_mask, 1<<i);
-	 i++;             
-  SysTick_delay_ms(100); 
-  if (i==4)
-      i=0;
+	uint16 count=0;
+	for (i=0; count<(1500*cycle); i++)
+	{
+		if(i==0)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_BLUE, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_PINK)|(1<<STEPPER_ORANGE)|(1<<STEPPER_YELLOW), STD_LOW);
+		}
+		else if(i==1)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_PINK, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_BLUE)|(1<<STEPPER_ORANGE)|(1<<STEPPER_YELLOW), STD_LOW);
+		}
+		else if(i==2)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_ORANGE, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_PINK)|(1<<STEPPER_BLUE)|(1<<STEPPER_YELLOW), STD_LOW);
+		}
+		else if(i==3)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_YELLOW, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_PINK)|(1<<STEPPER_ORANGE)|(1<<STEPPER_BLUE), STD_LOW);
+			i=0;
+		}
+		count++; 
+		SysTick_delay_ms(5);
+	}
 }
-}
 
 
 
-void StepperMotor_AntiClkWise(Dio_PortName port_name, uint8 pins_mask,float32 cycle)
+void StepperMotor_AntiClkWise(float32 cycle)
 {
 	uint8 i=0;
-for (float32 j=0; j<cycle; j++)
-{
-	DIO_WritePort( port_name,  pins_mask, 8>>i);
-	 i++;             
-  SysTick_delay_ms(100); 
-  if (i==4)
-      i=0;
-}
+	uint16 count=0;
+	for (i=0; count<(1500*cycle); i++)
+	{
+		if(i==0)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_YELLOW, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_PINK)|(1<<STEPPER_ORANGE)|(1<<STEPPER_BLUE), STD_LOW);
+		}
+		else if(i==1)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_ORANGE, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_BLUE)|(1<<STEPPER_PINK)|(1<<STEPPER_YELLOW), STD_LOW);
+		}
+		else if(i==2)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_PINK, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_ORANGE)|(1<<STEPPER_BLUE)|(1<<STEPPER_YELLOW), STD_LOW);
+		}
+		else if(i==3)
+		{
+			DIO_WritePort(STEPPER_PORT,1<<STEPPER_BLUE, STD_HIGH);
+			DIO_WritePort(STEPPER_PORT,(1<<STEPPER_PINK)|(1<<STEPPER_ORANGE)|(1<<STEPPER_YELLOW), STD_LOW);
+			i=0;
+		}
+		count++; 
+		SysTick_delay_ms(20);
+	}
 }
