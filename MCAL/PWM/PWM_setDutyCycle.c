3f1ed100328d64260ab5e18dbdb152e9a72ca04e
amir <amir.r.zarif@hotmail.com>
2019-05-15 00:17:40 +0200

    fixing PWM duty cycle function

--- MCAL/PWM/PWM_setDutyCycle.c
+++ MCAL/PWM/PWM_setDutyCycle.c
@@ -16,39 +16,54 @@ void PWM_SetDuty(PWMModule module, PWMChannel channel, uint16 duty)
 				case PWM0 :
 					period = PWM0_0_LOAD_R;
 					PWM0_0_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_0_CMPA_R = period -2;
 					break;
 				case PWM1 :
 					period = PWM0_0_LOAD_R;
 					PWM0_0_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_0_CMPB_R = period -2;
 					break;
 				case PWM2 :
 					period = PWM0_1_LOAD_R;
 					PWM0_1_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-					
+					if(duty == 0) 
+						PWM0_1_CMPA_R = period -2;					
 					break;
 				case PWM3 :
 					period = PWM0_1_LOAD_R;
 					PWM0_1_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_1_CMPB_R = period -2;		
 					
 					break;
 				case PWM4 :
 					period = PWM0_2_LOAD_R;
 					PWM0_2_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_2_CMPA_R = period -2;
 				
 					break;
 				case PWM5 :
 					period = PWM0_2_LOAD_R;
 					PWM0_2_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_2_CMPB_R = period -2;
 				
 					break;
 				case PWM6 :
 					period = PWM0_3_LOAD_R;
 					PWM0_3_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_3_CMPA_R = period -2;
 					
 					break;
 				case PWM7 :
 					period = PWM0_3_LOAD_R;
 					PWM0_3_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
+					if(duty == 0) 
+						PWM0_3_CMPB_R = period -2;
 					
 					break;
 				
@@ -63,43 +78,51 @@ void PWM_SetDuty(PWMModule module, PWMChannel channel, uint16 duty)
 				case PWM0 :
 					period = PWM1_0_LOAD_R;
 					PWM1_0_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-					
+					if(duty == 0) 
+						PWM1_0_CMPA_R = period -2;
 					break;
 				case PWM1 :
 					period = PWM1_0_LOAD_R;
 					PWM1_0_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
+					if(duty == 0) 
+						PWM1_0_CMPB_R = period -2;
 					break;
 				case PWM2 :
 					period = PWM1_1_LOAD_R;
 					PWM1_1_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
+					if(duty == 0) 
+						PWM1_1_CMPA_R = period -2;
 					break;
 				case PWM3 :
 					period = PWM1_1_LOAD_R;
 					PWM1_1_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
+					if(duty == 0) 
+						PWM1_1_CMPB_R = period -2;
 					break;
 				case PWM4 :
 					period = PWM1_2_LOAD_R;
 					PWM1_2_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
+					if(duty == 0) 
+						PWM1_2_CMPA_R = period -2;
 					break;
 				case PWM5 :
 					period = PWM1_2_LOAD_R;
 					PWM1_2_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
+					if(duty == 0) 
+						PWM1_2_CMPB_R = period -2;
 					break;
 				case PWM6 :
 					period = PWM1_3_LOAD_R;
 					PWM1_3_CMPA_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
-					break;
+					if(duty == 0) 
+						PWM1_3_CMPA_R = period -2;
+					break;	
 				
 				case PWM7 :
 					period = PWM1_3_LOAD_R;
 					PWM1_3_CMPB_R = (uint16)(period * (1 - (duty / 100.0)) - 1);
-				
+					if(duty == 0) 
+						PWM1_3_CMPB_R = period -2;
 					break;
 				
 				default:
